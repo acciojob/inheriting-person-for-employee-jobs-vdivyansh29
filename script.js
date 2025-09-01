ce2ac476d0e1147563073fc05cc8573b58502a94
Divyansh Verma <divyanshmotog5g@gmail.com>
2025-09-01 13:22:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ class Person{
     }
     
     greet(){
-        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`)
     }
 }
 
